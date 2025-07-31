2501eb283c1751bba9d316ee750b31dac763bb04
Marlon Quilante <marlonquilante@gmail.com>
2025-07-31 00:26:45 -0300

    Implementado o módulo de Produtos no ASP.NET

---
+++ ControleDeBar.WebApp/Models/ProdutoViewModel.cs
@@ -0,0 +1,84 @@
+﻿using ControleDeBar.Dominio.ModuloProduto;
+
+namespace ControleDeBar.WebApp.Models
+{
+    public class VisualizarProdutosViewModel
+    {
+        public List<DetalhesProdutoViewModel> Registros { get; set; }
+
+        public VisualizarProdutosViewModel()
+        {
+        }
+
+        public VisualizarProdutosViewModel(List<Produto> produtos)
+        {
+            Registros = new List<DetalhesProdutoViewModel>();
+
+            foreach (Produto p in produtos)
+            {
+                DetalhesProdutoViewModel detalhesVM = new DetalhesProdutoViewModel(p.Id, p.Nome, p.Preco);
+                Registros.Add(detalhesVM);
+            }
+        }
+    }
+
+    public class DetalhesProdutoViewModel
+    {
+        public int Id { get; set; }
+        public string Nome { get; set; }
+        public decimal Preco { get; set; }
+
+        public DetalhesProdutoViewModel() { }
+
+        public DetalhesProdutoViewModel(int id, string nome, decimal preco)
+        {
+            Id = id;
+            Nome = nome;
+            Preco = preco;
+        }
+    }
+
+    public class CadastrarProdutoViewModel
+    {
+        public string Nome { get; set; }
+        public decimal Preco { get; set; }
+
+        public CadastrarProdutoViewModel() { }
+
+        public CadastrarProdutoViewModel(string nome, decimal preco)
+        {
+            Nome = nome;
+            Preco = preco;
+        }
+    }
+
+    public class EditarProdutoViewModel
+    {
+        public int Id { get; set; }
+        public string Nome { get; set; }
+        public decimal Preco { get; set; }
+
+        public EditarProdutoViewModel() { }
+
+        public EditarProdutoViewModel(int id, string nome, decimal preco)
+        {
+            Id = id;
+            Nome = nome;
+            Preco = preco;
+        }
+    }
+
+    public class ExcluirProdutoViewModel
+    {
+        public int Id { get; set; }
+        public string Nome { get; set; }
+
+        public ExcluirProdutoViewModel() { }
+
+        public ExcluirProdutoViewModel(int id, string nome)
+        {
+            Id = id;
+            Nome = nome;
+        }
+    }
+}
